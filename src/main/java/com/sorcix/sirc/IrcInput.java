3f3114659c14520d49cec79f9c08db72b255613e
jamoBox <petewicken@gmail.com>
2013-12-24 11:11:48 +0000

    Minor code format fix

--- src/main/java/com/sorcix/sirc/IrcInput.java
+++ src/main/java/com/sorcix/sirc/IrcInput.java
@@ -119,7 +119,7 @@ final class IrcInput extends Thread {
 		} catch (final IOException ex) {
 			this.irc.setConnected(false);
 		} catch (final Exception ex) {
-                        IrcDebug.log("Exception " + ex + " on: " + line);
+			IrcDebug.log("Exception " + ex + " on: " + line);
 			ex.printStackTrace();
 		}
 		// when reaching this, we are disconnected
